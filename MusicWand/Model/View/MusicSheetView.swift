20955147d6750f8d8e578ad46ffc11fb5fa62f3b
minierparedes <3thancr0wn@gmail.com>
2020-09-18 16:07:25 +0900

    Updated: MusicSheetView

--- MusicWand/Model/View/MusicSheetView.swift
+++ MusicWand/Model/View/MusicSheetView.swift
@@ -25,46 +25,47 @@ struct MusicSheetView: View {
                         ForEach(allTrack , id: \.self){ score in
                             NavigationLink(destination: ScoreView(trackData: score, scoreModel: ScoreModel(inputnotes: Array(score.song)))){
                                 Button( action: {
+                                    //self.sequencer.playPause()
                                     self.playPauseButton.toggle()
-                                      }, label: {
-                                        PlayPauseButton(active: self.playPauseButton)
-                                                   }).buttonStyle(PlainButtonStyle()) .padding()
-                                //Image(systemName: "play.circle").resizable().frame(width: 35, height: 35).padding()
+                                }, label: {
+                                    PlayPauseButton(active: self.playPauseButton)
+                                }).buttonStyle(PlainButtonStyle()) .padding()
+                                
                                 ScoreRow(score: score)
-                
+                                
                             }
-                             
+                            
                         }
-                     .onDelete{ indexSet in
-                        let index = indexSet.first
-                        let id = self.allTrack[index!].trackID
-                        let track = self.musicStores.findTrack(id)
-                        self.allTrack = self.allTrack.enumerated().filter{!indexSet.contains($0.offset)}.map{$0.element}
-                        
-                        try! self.musicStores.realm.write{
-                            self.musicStores.realm.delete(track!)
+                        .onDelete{ indexSet in
+                            let index = indexSet.first
+                            let id = self.allTrack[index!].trackID
+                            let track = self.musicStores.findTrack(id)
+                            self.allTrack = self.allTrack.enumerated().filter{!indexSet.contains($0.offset)}.map{$0.element}
+                            
+                            try! self.musicStores.realm.write{
+                                self.musicStores.realm.delete(track!)
                             }
                         }
                     }
                     Button(action: {
                         self.showsAlert = true
                     },label: {Image(systemName: "plus.circle.fill").resizable().frame(width: 55, height: 55)})
-                    .padding(20)
+                        .padding(20)
                 }.alert(isPresented: $showsAlert, TextAlert(title: "Set New Track Title", action: {
                     let trackTitle = ($0 ?? "")
                     if trackTitle != "" {
-                    let track = self.musicStores.makeTrack(trackTitle)
+                        let track = self.musicStores.makeTrack(trackTitle)
                         self.musicStores.addTrack(track)
                         self.allTrack = Array(musicStore.store.realm.objects(musicTrack.self).freeze())
                         self.allNote = Array(musicStore.store.realm.objects(note.self).freeze())
                     }
                     self.showsAlert = false
                 })).navigationBarTitle(Text("Scores"))
-            }
-            
-
+        }
+        
+        
     }
-
+    
     func deleteRow(at indexSet: IndexSet){
     }
     
